ac054054751402c2887d1e8100aaa97df2f94130
Mohmed Nabil <mohmedn424@gmail.com>
2024-07-29 14:48:37 +0300

    commiting minor changes to connect the backend

--- src/components/CurrentHistory.jsx
+++ src/components/CurrentHistory.jsx
@@ -1,10 +1,17 @@
 import { memo } from 'react';
 import PreviousIllnessesConditions from './PreviousIllnessesConditions';
+import { Select } from 'antd';
 
 export default memo(function CurrentHistory() {
   return (
     <div className="current-history-wrapper">
       <PreviousIllnessesConditions />
+
+      <div className="current-examintion">
+        <h1 style={{ textAlign: 'center' }}>Current Examition</h1>
+        <br />
+        <Select style={{ width: '100%' }} />
+      </div>
     </div>
   );
 });
--- src/components/PreviousIllnessesConditions.jsx
+++ src/components/PreviousIllnessesConditions.jsx
@@ -1,19 +1,10 @@
-import {
-  Collapse,
-  ConfigProvider,
-  Form,
-  Input,
-  Radio,
-  Select,
-} from 'antd';
+import { Collapse, ConfigProvider } from 'antd';
 import Allergies from './Allergies';
 import Conditions from './Conditions';
 import PastSurgicalHistory from './PastSurgicalHistory';
 import ReviewOfSystems from './ReviewOfSystems';
 
 export default function PreviousIllnessesConditions() {
-  const formFinish = (e) => {};
-
   return (
     <>
       <h2>previous illnesses or conditions</h2>
